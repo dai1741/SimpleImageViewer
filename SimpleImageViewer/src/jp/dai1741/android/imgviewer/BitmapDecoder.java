72f668db6306743a6c36aa05a746ea8c012e5e2e
dai1741 <fujita.dai@gmail.com>
2011-12-18 01:50:32 +0900

    Add comments

--- SimpleImageViewer/src/jp/dai1741/android/imgviewer/BitmapDecoder.java
+++ SimpleImageViewer/src/jp/dai1741/android/imgviewer/BitmapDecoder.java
@@ -9,14 +9,31 @@ import android.net.Uri;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * ビットマップを生成する情報を保持し、適宜デコードを行うインターフェース。
+ */
 public interface BitmapDecoder {
-    
+
+    /**
+     * 保持している情報をデコードし、ビットマップを作成する。
+     * 
+     * @param op
+     * @return ビットマップかnull
+     * @throws IOException
+     */
     Bitmap decode(BitmapFactory.Options op) throws IOException;
-    
+
+    /**
+     * ファイルをデコードする{@code BitmapDecoder}。
+     */
     public static class FileBitmapDecoder implements BitmapDecoder {
 
         private final String mFilePath;
-        
+
+        /**
+         * @param filePath
+         *            null可
+         */
         public FileBitmapDecoder(String filePath) {
             mFilePath = filePath;
         }
@@ -25,19 +42,26 @@ public interface BitmapDecoder {
         public Bitmap decode(Options op) {
             return BitmapFactory.decodeFile(mFilePath, op);
         }
-        
+
     }
-    
+
+    /**
+     * {@code ContentResolver}が解釈するuriをデコードする{@code BitmapDecoder}。
+     */
     public static class ContentBitmapDecoder implements BitmapDecoder {
 
         private final Context mContext;
         private final Uri mContentUri;
-        
+
         public ContentBitmapDecoder(Context context, Uri contentUri) {
             mContext = context;
             mContentUri = contentUri;
         }
 
+        /**
+         * @throws IOException
+         *             contentUriが不正のとき
+         */
         @Override
         public Bitmap decode(Options op) throws IOException {
             InputStream stream;
@@ -46,6 +70,6 @@ public interface BitmapDecoder {
             stream.close();
             return ret;
         }
-        
+
     }
 }
